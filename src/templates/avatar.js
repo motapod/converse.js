908674f8aca4dbebe8750c723755f63e992a89f4
JC Brand <jc@opkode.com>
2020-09-26 21:07:46 +0200

    Always render avatar via lit-html

--- src/templates/avatar.js
+++ src/templates/avatar.js
@@ -1,6 +1,25 @@
 import { html } from "lit-html";
+import { unsafeSVG } from 'lit-html/directives/unsafe-svg.js';
+import xss from "xss/dist/xss";
+
+const whitelist_opts = {
+    'whiteList': {
+        'svg': ['xmlns', 'class', 'width', 'height'],
+        'image': ['width', 'height', 'preserveAspectRatio', 'href']
+    }
+};
+
+const getImgHref = (image, image_type) => {
+    return image.startsWith('data:') ? image : `data:${image_type};base64,${image}`;
+}
+
+const tpl_svg = (o) => xss.filterXSS(
+    `<image width="${o.width}" height="${o.height}" preserveAspectRatio="xMidYMid meet" href="${getImgHref(o.image, o.image_type)}"/>`,
+    whitelist_opts
+);
 
 export default  (o) => html`
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="${o.classes}" width="${o.width}" height="${o.height}">
-        <image width="${o.width}" height="${o.height}" preserveAspectRatio="xMidYMid meet" href="data:${o.image_type};base64,${o.image}"/>
+    <svg xmlns="http://www.w3.org/2000/svg" class="avatar ${o.classes}" width="${o.width}" height="${o.height}">
+        ${ unsafeSVG(tpl_svg(o)) }
     </svg>`;
+
